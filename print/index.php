9d6f5e39717f62d33c7c286edd560b0a2e9150f5
HotStealer <karl.podra@khk.ee>
2018-12-13 15:08:55 +0200

    Väljastus - PHP ja HTML

--- print/index.php
+++ print/index.php
@@ -1,7 +1,12 @@
+<!doctype html>
+<html>
+<head>
+    <title>PHP alused - väljund</title>
+</head>
+<body>
+<h4>Väljastus</h4>
 <?php
-//teksti väljastus print käsuga
-print 'See on esimene lause PHP keeles'.'<br>';
-print 'See on teine lause PHP keeles'.'<br>';
-//teksti väljastus echo käsuga
-echo 'See tekst on echo abil väljastatud'.'<br>';
-
+echo 'See tekst on HTMLi sees'.'<br>';
+?>
+</body>
+</html>
