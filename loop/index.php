d5503bdf09e8fad37fa222e924f902f9f01888a7
HotStealer <karl.podra@khk.ee>
2018-12-13 16:44:22 +0200

    PHP tsüklid - for tabeli veergude loomiseks

--- loop/index.php
+++ loop/index.php
@@ -13,14 +13,17 @@ echo '<h4>Tsüklid - for</h4>';
 
 echo '<table>';
 for($rida = 1; $rida <= 5; $rida++){
-    $varv = '#';
-    for($kord = 1; $kord <= 6; $kord++){
-        $varv = $varv.dechex(rand(0,15));
-    }
+
     echo '<tr>';
+        for($veerg = 1; $veerg <=5; $veerg++){
+            $varv = '#';
+            for($kord = 1; $kord <= 6; $kord++){
+                $varv = $varv.dechex(rand(0,15));
+            }
         echo '<td style="background: '.$varv.'">';
         echo '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp';
         echo '</td>';
+        }
     echo '</tr>';
 }
 echo '</table>';
