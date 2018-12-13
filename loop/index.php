b0b6c87a328140078018c625da9ff712e3b7768f
HotStealer <karl.podra@khk.ee>
2018-12-13 16:40:45 +0200

    PHP tsüklid - for tabeli loomiseks

--- loop/index.php
+++ loop/index.php
@@ -1,4 +1,5 @@
 <?php
+header('Refresh: 1');
 // for
 /*
  * for($jm =alg; $jm < lopp; $jm++){
@@ -6,13 +7,20 @@
  * kui tsükli $jm väärtus on väiksem kui lõppväärtus
  * }
  */
-$varv = '';
+
 echo '<h4>Tsüklid - for</h4>';
-for($arv = 1; $arv <= 10; $arv++){
-    if($arv % 2 == 0){
-        $varv ='green';
-    }else{
-        $varv='red';
+
+
+echo '<table>';
+for($rida = 1; $rida <= 5; $rida++){
+    $varv = '#';
+    for($kord = 1; $kord <= 6; $kord++){
+        $varv = $varv.dechex(rand(0,15));
     }
-    echo '<div style="color:'.$varv.'">'.$arv.'</div>';
+    echo '<tr>';
+        echo '<td style="background: '.$varv.'">';
+        echo '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp';
+        echo '</td>';
+    echo '</tr>';
 }
+echo '</table>';
