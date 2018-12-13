7fe1c161a56af28d6dd26d07d2547dab1e683eba
HotStealer <karl.podra@khk.ee>
2018-12-13 16:28:59 +0200

    PHP tsüklid - For tsükkel ifiga

---
+++ loop/index.php
@@ -0,0 +1,18 @@
+<?php
+// for
+/*
+ * for($jm =alg; $jm < lopp; $jm++){
+ * tegevused, mis sooritatakse nii kaua
+ * kui tsükli $jm väärtus on väiksem kui lõppväärtus
+ * }
+ */
+$varv = '';
+echo '<h4>Tsüklid - for</h4>';
+for($arv = 1; $arv <= 10; $arv++){
+    if($arv % 2 == 0){
+        $varv ='green';
+    }else{
+        $varv='red';
+    }
+    echo '<div style="color:'.$varv.'">'.$arv.'</div>';
+}
