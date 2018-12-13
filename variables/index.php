5cfbc4a93b088bc57fa823d61d40ddf533dc58c0
HotStealer <karl.podra@khk.ee>
2018-12-13 15:28:33 +0200

    PHP muutujad

---
+++ variables/index.php
@@ -0,0 +1,15 @@
+<?php
+// muutujate defineerimine
+// $muutujaNimi = väärtus;
+
+$taisArv = 7;
+$reaalArv = 3.14;
+$sone = 'Lars';
+$toevaartusTrue = true;
+$toevaartusFalse = false;
+
+echo 'Täisarv = '.$taisArv.'<br>';
+echo 'Reaalarv = '.$reaalArv.'<br>';
+echo 'Sõne = '.$sone.'<br>';
+echo 'Toeväärtus true = '.$toevaartusTrue.'<br>';
+echo 'Toeväärtus false = '.$toevaartusFalse.'<br>';
