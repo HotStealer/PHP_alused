76a996a65912250a1d0c5f43f29de90d172d39b0
HotStealer <karl.podra@khk.ee>
2018-12-13 16:18:17 +0200

    PHP tingimused - Switchi kasutamine

--- condition/index.php
+++ condition/index.php
@@ -6,19 +6,19 @@
 // tegevused, mis toimuvad kui tingimus ei kehti
 // }
 echo '<h4>Tingimuslaused</h4>';
-$arv = rand(0,100); // juhuslik täisarv vahemikus 0-100
-$varv = '';
-if ($arv >= 0 and $arv < 25){
-    // roheline
-    $varv = 'green';
-}else if ($arv >= 25 and $arv <50){
-    // punane
-    $varv = 'red';
-}else if ($arv >= 50 and $arv <75){
-    // sinine
-    $varv = 'blue';
-}else if ($arv >= 75 and $arv <101){
-    // kollane
-    $varv = 'yellow';
+$foor = 'roheline';
+
+switch ($foor){
+    case 'punane':
+        echo '<p style="color:red">Seis!</p>';
+        break;
+    case 'kollane':
+        echo '<p style="color:yellow">Tähelepanu!</p>';
+        break;
+    case 'roheline':
+        echo '<p style="color:green">Läks!</p>';
+        break;
+    default:
+        echo '<p>Foor ei tööta</p>';
+        break;
 }
-echo '<p style="color:'.$varv.'">'.$arv.'</p>';
