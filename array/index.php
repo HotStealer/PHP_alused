f6ec20775483a019b0f9fc7a153c749fa1469292
HotStealer <karl.podra@khk.ee>
2018-12-18 12:17:51 +0200

    massiivid - väljastamine ja elementide töötlus

--- array/index.php
+++ array/index.php
@@ -4,10 +4,19 @@ $kasutajad = array(); // tühi massiiv
 $kasutajad[] = 'alice';
 $kasutajad[] = 'bob';
 array_push($kasutajad, 'lucy');
-
 $lisaKasutajad = array('mark','george'); //andmetega massiiv
-echo '<pre>';
-print_r($kasutajad);
-print_r($lisaKasutajad);
-echo '</pre>';
-// var_dump($kasutajad);
+
+//massiivi pikkus
+$kasutajadKokku = count($kasutajad);
+for($i = 0; $i < $kasutajadKokku; $i++){
+    echo $kasutajad[$i].'<br>';
+}
+array_shift($kasutajad);
+for($i = 0; $i <$kasutajadKokku; $i++){
+    echo $kasutajad[$i].'<br>';
+}
+
+$uuedKasutajad = array_merge($kasutajad, $lisaKasutajad);
+for($i = 0; $i <$uuedKasutajad; $i++){
+    echo $uuedKasutajad[$i].'<br>';
+}
