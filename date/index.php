bb1437990bf62289acfbb3156801b732d463b6c0
Karl <karl.podra@khk.ee>
2019-01-13 18:42:09 +0200

    Massiivi kasutamine aja väljastuses

--- date/index.php
+++ date/index.php
@@ -1,4 +1,10 @@
 <?php
 echo time().'<br>';
-echo date('d.m.Y G:i' , time());
+echo date('d.m.Y G:i' , time()).'<br>';
 date_default_timezone_set('Europe/Tallinn');
+$eesti_kuud = array(1=>'jaanuar', 'veebruar', 'märts', 'aprill', 'mai', 'juuni', 'juuli', 'august', 'september', 'oktoober', 'november', 'detsember');
+$paev = date('d');
+$kuu = $eesti_kuud[date('n')];
+$aasta = date('Y');
+echo $paev.'.'.$kuu.' '.$aasta;
+?>
