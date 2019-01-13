666ccdab1d76c2c7b86e7aec5f2ae750eec248af
Karl <karl.podra@khk.ee>
2019-01-13 18:49:08 +0200

    Ajale sekundite lisamine ja strtotime funktsioon

--- date/index.php
+++ date/index.php
@@ -9,4 +9,8 @@ $aasta = date('Y');
 echo $paev.'.'.$kuu.' '.$aasta.'<br>';
 $sp = mktime(18,14,4,11,21,1979);
 echo date('d.m.Y', $sp).'<br>';
+echo date('d.m.Y G:i' , time()+2000).'<br>';
+echo date('d.m.Y G:i' , time()+2500*150).'<br>';
+echo date('d.m.Y G:i' , time()+59*120*954).'<br>';
+echo strtotime("+12 week 5 days 2 hours 20 seconds").'<br>';
 ?>
