9bba2468d6467e2b61d2a3f648c62e1fa00332a7
HotStealer <karl.podra@khk.ee>
2019-01-22 13:50:29 +0200

    tekstifunktsioonid - ülesanne 9.4

--- text/index.php
+++ text/index.php
@@ -1,5 +1,6 @@
 <?php
-$nimi = $_GET['nimi'];
-$otsi = array('noob','kurat','raisk');
-$asenda = '****';
-echo str_replace($otsi, $asenda, $nimi);
+$ees = $_GET['eesnimi'];
+$pere = $_GET['perenimi'];
+$otsi = array('ä','õ','ö','ü');
+$asenda = array('a','o','o','y');
+echo strtolower(str_replace($otsi, $asenda, $ees)).'.'.strtolower(str_replace($otsi, $asenda, $pere)).'@hkhk.edu.ee';
