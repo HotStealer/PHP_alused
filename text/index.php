b7e93e9cf15c8eb7b0fd9973484c6960346d809b
HotStealer <karl.podra@khk.ee>
2019-01-22 12:56:08 +0200

    tekstifunktsioonid - ülesanne 9.1

--- text/index.php
+++ text/index.php
@@ -1,7 +1,6 @@
 <?php
-$tekst = 'Musta lehma saba musta lehma taga, valge lehma saba valge lehma taga';
-$otsi = array('lehm', 'saba', 'taga');
-$asenda = array('koer','sarv','ees');
-echo substr_replace($otsi, $asenda,$tekst);
+$nimi = $_GET['nimi'];
+
+echo 'Tere, '.ucfirst(strtolower($nimi));
 
 
