9901e0f1558f355dd3ab66e129e6543faf8bb0ac
Karl <karl.podra@khk.ee>
2019-01-27 22:28:39 +0200

    sessioon - loodud kasutaja andmete kontroll andmebaasist

--- session/acts/login.php
+++ session/acts/login.php
@@ -1,5 +1,10 @@
 <?php
+require_once '../db/conf.php';
+require_once '../db/db_fnk.php';
 $username = $_GET['uname'];
 $password = $_GET['upass'];
 
-echo $username.' '.$password;
+$ikt = connect_db(DBHOST, DBUSER, DBPASS, DBNAME);
+$sql = 'SELECT * FROM user WHERE username="'.$username.'"AND password="'.md5($password).'"';
+$users = getData($sql, $ikt);
+echo $users[0]['first_name'].' '.$users[0]['last_name'];
